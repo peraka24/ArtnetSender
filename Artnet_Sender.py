401979f6e7b177a77d2482ba12b0c4d759a46652
peraka24 <77302058+peraka24@users.noreply.github.com>
2023-03-01 12:40:40 +0100

    Update Artnet_Sender.py

--- Artnet_Sender.py
+++ Artnet_Sender.py
@@ -1,27 +1,24 @@
 import stupidArtnet
-import time
 
-TARGET_IP = "1.0.0.124"  # Server IP address
-TARGET_UNIVERSE = 1
+TARGET_IP = "1.0.0.124"  # Server IP address, put in the IP adresse you want to send to
+TARGET_UNIVERSE = 1      # Universe you want to send
 
-
-sender = stupidArtnet.StupidArtnet(TARGET_IP, TARGET_UNIVERSE)
+sender = stupidArtnet.StupidArtnet(TARGET_IP, TARGET_UNIVERSE)   
 sender.start()
 
 def send_data_to_server(data):
     sender.set(data)
-    sender.show()
     #sender.set_single_value(1, 255) # Just for testing, remove later
 
 listen_server = stupidArtnet.StupidArtnetServer()
 
 universe_0_listener = listen_server.register_listener(
-universe=0,
-callback_function=send_data_to_server  # Funksjon som kjøres automatisk når det mottas ArtNet
+universe=0,      #listen to universe 0
+callback_function=send_data_to_server  # Function that sends data automatic when recieving Artnet
 )
 
 while True:
-    sender.see_buffer()
+    sender.see_buffer()   #Shows Artnet values
     
 sender.stop()
-print("done")  #Just to check the program
+print("done")  #Just to se if program is done
